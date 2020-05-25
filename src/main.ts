c9789ec0283a3c72bf1bbdb8277fa8ba9d8a1dd0
Nikhil Mahendran <m_nikhil@outlook.com>
2020-05-25 15:40:50 +0530

    add openapi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Swagger configuations
+   */
+  const options = new DocumentBuilder()
+    .setTitle('Auth Server')
+    .setDescription('Auth Server API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
