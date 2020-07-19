3ed8edb1a3b3c6925e455ff0dbc8426944527ad8
Nikhil <m-nikhil@users.noreply.github.com>
2020-07-19 17:59:45 +0530

    Fix migration (#13)
    * fix migrations

--- src/migrations/1594753901230-userUseUUID.ts
+++ src/migrations/1594753901230-userUseUUID.ts
@@ -1,20 +1,37 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class userUseUUID1594753901230 implements MigrationInterface {
-    name = 'userUseUUID1594753901230'
+export class UserUseUUID1594753901230 implements MigrationInterface {
+  name = 'UserUseUUID1594753901230';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "PK_cace4a159ff9f2512dd42373760"`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "id"`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" ADD "id" uuid NOT NULL DEFAULT uuid_generate_v4()`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "PK_cace4a159ff9f2512dd42373760"`,
+      undefined,
+    );
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "id"`, undefined);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "id" uuid NOT NULL DEFAULT uuid_generate_v4()`,
+      undefined,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")`,
+      undefined,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "PK_cace4a159ff9f2512dd42373760"`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "id"`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" ADD "id" SERIAL NOT NULL`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")`, undefined);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "PK_cace4a159ff9f2512dd42373760"`,
+      undefined,
+    );
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "id"`, undefined);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "id" SERIAL NOT NULL`,
+      undefined,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")`,
+      undefined,
+    );
+  }
 }
